d2ababebb7d5e29570338db49ac2dad036457c17
vikashkumar6207 <vktd828403@gmail.com>
2024-06-09 12:49:29 +0530

    Schema

--- models/bookModel.js
+++ models/bookModel.js
@@ -4,29 +4,54 @@ const bookSchema = new mongoose.Schema({
   // // Define the 'title' field
   // // The data type of this field is a string
   // // This field is required
+  title : {
+    type : String,
+    required : true,
+  },
   // }
   // 2) {
   // // Define the 'author' field
   // // The data type of this field is a string
   // // This field is required
+  author : {
+      type : String,
+      required : true,
+  },
   // }
   // 3) {
   // // Define the 'ISBN' field
   // // The data type of this field is a string
   // // This field is required
   // // Each ISBN must be unique
+    ISBN : {
+      type : String,
+      required : true,
+      unique: [true, "ISBN should be unique"],
+    },
   // }
   // 4) {
   //   // Define the 'publishedYear' field
   //   // The data type of this field is a number
   //   // This field is required
+  publishedYear : {
+    type : Number,
+    required : true,
+  },
   // }
-  // 5) Define the 'genre' field
+  // 5) Define the 'genre' field 
   // The data type of this field is a string
+  genre : {
+    type : String,
+  },
   // 6) {
   //   // Define the 'copiesAvailable' field
   //   // The data type of this field is a number
   //  // The default value is 1
+  copiesAvailable : {
+    type : Number,
+    default : 1,
+
+  }
   // }
 });
 
